fe4032bb59cd9046a2aeb41437ca515ebda71fba
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2017-12-08 15:24:37 +0300

    ok

---
+++ CreditApp/CreditApp/CreditApp/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace CreditApp.Core.UWP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
