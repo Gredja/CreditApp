2ff2aabf25b9a769f3ff804d8b58c157f713c38c
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2017-12-08 16:25:14 +0300

    ok

---
+++ CreditApp/CreditApp.Core/ViewModels/HomeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CreditApp.Core.ViewModels.Base;
+
+namespace CreditApp.Core.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        private string _name;
+
+
+        public HomeViewModel()
+        {
+            // _databaseManager = databaseManager;
+
+
+            Name = "AlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlexAlex";
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
---
+++ CreditApp/CreditApp/CreditApp/Views/HomeView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using CreditApp.Core.ViewModels;
+using Ninject;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CreditApp.Core.UWP.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeView : ContentPage
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+
+            BindingContext = Bootstrapper.Instance.Create<HomeViewModel>();
+        }
+    }
+}
