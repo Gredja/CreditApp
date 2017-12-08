369b89fcfec980623e3ca99f8af2eb46a3f64337
SOFTEQ\agridiushko <alexey.gridiushko@softeq.com>
2017-12-08 18:04:46 +0300

    ok

---
+++ CreditApp/CreditApp.Infrastructure.Data/Repositories/DatabaseRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using CreditApp.Infrastructure.Data.Repositories.Interfaces;
+
+namespace CreditApp.Infrastructure.Data.Repositories
+{
+  public  class DatabaseRepository : RepositoryBase , IDatabaseRepository
+    {
+        public DatabaseRepository(ISqLiteConnection sqLiteConnection) : base(sqLiteConnection)
+        {
+        }
+
+        public Task CreateDatabaseAsync()
+        {
+            //TODO
+            return null;
+        }
+    }
+}
